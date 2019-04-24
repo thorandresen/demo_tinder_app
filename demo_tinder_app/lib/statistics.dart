452d0569cac394fc9092382f12d312ee3cefa79c
ThorAndresen <au576984@uni.au.dk>
2019-04-24 08:59:03 +0200

    added a class for holding graph data and added the dependency for charts

--- demo_tinder_app/lib/statistics.dart
+++ demo_tinder_app/lib/statistics.dart
@@ -1,8 +1,9 @@
 import 'package:demo_tinder_app/drawermenu.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart';
 
-class StatisticsPage extends StatefulWidget{
+class StatisticsPage extends StatefulWidget {
   StatisticsPageState createState() => new StatisticsPageState();
 }
 
@@ -13,6 +14,16 @@ class StatisticsPageState extends State<StatisticsPage> {
       appBar: AppBar(
         centerTitle: true,
         title: Text('Statistikker'),
+      ),
+      body: new Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          new Row(
+            children: <Widget>[
+              Text('Partifordeling af likede politikkere'),
+            ],
+          )
+        ],
       ),
       drawer: DrawerMenu().drawerMenu(context),
     );
