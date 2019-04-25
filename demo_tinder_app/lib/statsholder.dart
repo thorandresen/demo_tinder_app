39c155d4e8a70d1796f7dfe59ced995cd69735bd
ThorAndresen <au576984@uni.au.dk>
2019-04-25 11:40:38 +0200

    it only saves the liked politicians now

--- demo_tinder_app/lib/statsholder.dart
+++ demo_tinder_app/lib/statsholder.dart
@@ -18,22 +18,41 @@ class StatsHolder {
   void _generateStatsMap() async {
     // Get the shared preference.
     prefs = await SharedPreferences.getInstance();
+    int counter = 0;
 
-    for (int i = 0; i < _collectionList.length; i++){
-      if (prefs.getString(_collectionList[i] + "Map") == null || prefs.getString(_collectionList[i] + "Map") == "" || prefs.getString(_collectionList[i] + "Map") == {}){
+    for (int i = 0; i < _collectionList.length; i++) {
+      if (prefs.getString(_collectionList[i] + "Map") == null ||
+          prefs.getString(_collectionList[i] + "Map") == "" ||
+          prefs.getString(_collectionList[i] + "Map") == {}) {
         print('STATSHOLDER: COLLECTION HAD NO LIKED PERSONS');
         return;
-      }
-      else{
-        final _politicianMap = await json.decode(
-            prefs.getString(_collectionList[i] + "Map") ?? "") ?? {};
-        
-        _statsMap.update(
-            _collectionList[i],
-            (update) =>  _politicianMap.length,
-          ifAbsent: () => _politicianMap.length
-        );
-        print('Statsmap was added with: ' + _collectionList[i] + " with length of: " + _politicianMap.length.toString());
+      } else {
+        final _politicianMap = await json
+                .decode(prefs.getString(_collectionList[i] + "Map") ?? "") ??
+            {};
+
+        var _polList = _politicianMap.values.toList();
+
+        if (_polList.length != 0 || _polList.length != null || _polList != {}) {
+          for (int j = 0; j < _polList.length; j++) {
+            print('Value of politician is: ' + _polList[j].toString());
+            if (_polList[j]) {
+              counter++;
+              _statsMap.update(_collectionList[i], (update) => counter,
+                  ifAbsent: () => counter);
+
+              print('Statsmap was added with: ' +
+                  _collectionList[i] +
+                  " with length of: " +
+                  counter.toString());
+            } else {
+              print('Poltician was not liked!');
+            }
+          }
+          counter = 0;
+        } else {
+          print('STATSHOLDER: no liked polticians');
+        }
       }
     }
   }
