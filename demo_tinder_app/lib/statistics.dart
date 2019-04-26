b6f7e5c02f32c2b421a1c989dcabdb8057498cd9
dhorhehe <thorg3@gmail.com>
2019-04-26 21:35:44 +0200

    few comments

--- demo_tinder_app/lib/statistics.dart
+++ demo_tinder_app/lib/statistics.dart
@@ -34,32 +34,37 @@ class StatisticsPageState extends State<StatisticsPage> {
                 style: TextStyle(fontSize: 20.0),
               ),
               new FutureBuilder<void>(
+                // Future builder for making sure that u piechart is only shown when it needs to.
                 future: _calculator(),
                 builder: (BuildContext context, AsyncSnapshot snapshot) {
-                  if(snapshot.data != null) {
+                  if (snapshot.data != null) {
                     return chart();
-                  }
-                  else {
+                  } else {
                     return new Container(
-                      child: Column(
-                        children: <Widget>[
-                          Text(''),
-                          new CircularProgressIndicator(),
-                          Text(''),
-                        new Text('Ingen politkkere er liket endnu... Back to swiping!', style: TextStyle(
-                        fontSize: 18.0,
-                      ),),
-                        ],
-                      )
-                    );
+                        child: Column(
+                      children: <Widget>[
+                        Text(''),
+                        new CircularProgressIndicator(),
+                        Text(''),
+                        new Text(
+                          'Ingen politkkere er liket endnu...',
+                          style: TextStyle(
+                            fontSize: 18.0,
+                          ),
+                        ),
+                      ],
+                    ));
                   }
                 },
               ),
               Text(''),
               Text(''),
-              Text('Flere stastikker på vej...', style: TextStyle(
-                color: Colors.grey,
-              ),)
+              Text(
+                'Flere stastikker på vej...',
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              )
             ],
           )
         ],
@@ -68,6 +73,7 @@ class StatisticsPageState extends State<StatisticsPage> {
     );
   }
 
+  /// Method for calculating whether it should show the chart.
   Future<void> _calculator() async {
     bool test = await sh.generateStatsMap();
     map = sh.statsMap;
@@ -75,15 +81,13 @@ class StatisticsPageState extends State<StatisticsPage> {
     if (test && map.isNotEmpty) {
       print('I am working!');
       return map;
-    }
-    else{
+    } else {
       print('I am not working!');
       return null;
     }
   }
 
-
-
+  /// Method for returning the piechart for showing amount of liked politicians in different parties.
   Widget chart() {
     return PieChart(
       dataMap: map,
