cd0c88ab2090117f06b7a12bbc3ea8edee3fdf13
dhorhehe <thorg3@gmail.com>
2019-04-27 11:09:55 +0200

    added more info the appdelegate.m

--- demo_tinder_app/ios/Runner/AppDelegate.m
+++ demo_tinder_app/ios/Runner/AppDelegate.m
@@ -1,11 +1,13 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
+@import Firebase; // this I have added.
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
+  [FIRApp configure]; // This I have added
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
