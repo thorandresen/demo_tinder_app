7a8861c6b279127e20eb7a9f3233a29001693f0f
ThorAndresen <au576984@uni.au.dk>
2019-05-01 08:54:47 +0200

    fixed build error in android, and added icon to savedpoliticans

--- demo_tinder_app/lib/savedpoliticians.dart
+++ demo_tinder_app/lib/savedpoliticians.dart
@@ -4,7 +4,7 @@ import 'package:demo_tinder_app/statsgenerator.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SavedPoliticiansPage extends StatefulWidget{
+class SavedPoliticiansPage extends StatefulWidget {
   SavedPoliticiansPageState createState() => new SavedPoliticiansPageState();
 }
 
@@ -28,35 +28,32 @@ class SavedPoliticiansPageState extends State<SavedPoliticiansPage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             new Expanded(
-            child: new Column(
-              children: <Widget>[
-                /// Dropdown menu
-                new FutureBuilder(
-                    future: _calculator(),
-                    builder: (BuildContext context, AsyncSnapshot snapshot) {
-                      if (snapshot.data != null) {
-                        return _buildMenu('Venligst vælg et parti');
-                      }
-                      else{
-                        return _buildMenu('Ingen politikere liket endnu');
-                      }
-                    }
-                ),
-                /// Listview
-                   new FutureBuilder(
+              child: new Column(
+                children: <Widget>[
+                  /// Dropdown menu
+                  new FutureBuilder(
+                      future: _calculator(),
+                      builder: (BuildContext context, AsyncSnapshot snapshot) {
+                        if (snapshot.data != null) {
+                          return _buildMenu('Venligst vælg et parti');
+                        } else {
+                          return _buildMenu('Ingen politikere liket endnu');
+                        }
+                      }),
+
+                  /// Listview
+                  new FutureBuilder(
                     future: _retrievePoliticians(),
                     builder: (BuildContext context, AsyncSnapshot snapshot) {
                       if (snapshot.data != null) {
                         return _buildList();
-                      }
-                      else{
+                      } else {
                         return Text('');
                       }
                     },
                   ),
-
-              ],
-            ),
+                ],
+              ),
             ),
           ],
         ),
@@ -68,44 +65,40 @@ class SavedPoliticiansPageState extends State<SavedPoliticiansPage> {
   /// Method used with the future builder for returning correct hint for the dropdown menu.
   Future<void> _calculator() async {
     bool test = await sh.generateStatsMap();
-    _collections = sh.statsMap.keys.toList(); // Retrieve all the collections with liked politicians.
-    if(test && _collections.isNotEmpty){
+    _collections = sh.statsMap.keys
+        .toList(); // Retrieve all the collections with liked politicians.
+    if (test && _collections.isNotEmpty) {
       return _collections;
-    }
-    else{
+    } else {
       return null;
     }
   }
 
   /// The method that retrieves the firestore number for all the correct politicians. This puts all the correct ints into the _likedPoliticans list.
   Future<bool> _retrievePoliticians() async {
-
-
-
     bool test = await sh.generateLikedPoliticianMap(_selectedCollection);
     if (test) {
       _likedPoliticans.clear(); // Clearing the liked politician.
 
       List<String> _likedPoliticansString = sh.likedPoliticiansList;
 
-     for(int i = 0; i < _likedPoliticansString.length; i++){
-       String cutString = _likedPoliticansString[i].substring(0,1);
-       int correctNumber = int.parse(cutString)-1;
-       _likedPoliticans.add(correctNumber);
-       print('This number was added to the list: ' + correctNumber.toString());
-     }
-     if(_likedPoliticans.length > 0) {
-       return true;
-     }
-    }
-    else{
+      for (int i = 0; i < _likedPoliticansString.length; i++) {
+        String cutString = _likedPoliticansString[i].substring(0, 1);
+        int correctNumber = int.parse(cutString) - 1;
+        _likedPoliticans.add(correctNumber);
+        print('This number was added to the list: ' + correctNumber.toString());
+      }
+      if (_likedPoliticans.length > 0) {
+        return true;
+      }
+    } else {
       print('Something went wrong in getting liked politcians.');
       return false;
     }
   }
 
   /// The method that actually builds the dropdown menu.
-  Widget _buildMenu(String hint){
+  Widget _buildMenu(String hint) {
     return DropdownButton(
       hint: Text(hint), // Not necessary for Option 1
       value: _selectedCollection,
@@ -126,24 +119,20 @@ class SavedPoliticiansPageState extends State<SavedPoliticiansPage> {
   /// For building the list.
   Widget _buildList() {
     return new StreamBuilder(
-        stream: Firestore.instance
-            .collection(_selectedCollection)
-            .snapshots(),
+        stream: Firestore.instance.collection(_selectedCollection).snapshots(),
         builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
           if (!snapshot.hasData) return new CircularProgressIndicator();
           return new ListView.builder(
-            shrinkWrap: true,
+              shrinkWrap: true,
               itemCount: _likedPoliticans.length,
               itemBuilder: (BuildContext context, int index) {
-                List<DocumentSnapshot> item = snapshot
-                    .data.documents;
-                  return new ListTile(
-                    title: Text(item[_likedPoliticans[index]]['name']),
-                    subtitle: Text(item[_likedPoliticans[index]]['partiName']),
-                  );
-              }
-          );
-        }
-    );
+                List<DocumentSnapshot> item = snapshot.data.documents;
+                return new ListTile(
+                  title: Text(item[_likedPoliticans[index]]['name']),
+                  subtitle: Text(item[_likedPoliticans[index]]['partiName']),
+                  trailing: new Icon(Icons.arrow_forward_ios),
+                );
+              });
+        });
   }
 }
